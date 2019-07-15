54c18c67069a9ac1b424843dedfc543063b7fe98
TENCENT\junwenli <junwenli@tencent.com>
2019-07-15 20:39:02 +0800

    include elementUI

--- src/pages/Index/template.vue
+++ src/pages/Index/template.vue
@@ -1,14 +1,15 @@
 <template>
   <div id="index">
-    首页
+    <p>首页</p>
+    <div>
+      <el-button type="danger">危险按钮</el-button>
+    </div>
   </div>
 </template>
 
 <script src="./template.js">
-
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style src="./template.css">
-
 </style>
